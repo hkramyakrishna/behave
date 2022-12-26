059b7464c16be48ff370df1ce1d1b5cee6a8d596
hkramyakrishna <114924528+hkramyakrishna@users.noreply.github.com>
2022-12-26 21:27:33 +0530

    Create sscript.sh

---
+++ sscript.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Installing behave"
+python_install()
+{
+sudo apt install python3
+if [ $? -ne 0 ]; then
+echo "error while installing"
+fi
+echo "python installed successfully"
+}
+bahave_install()
+{
+sudo apt install python3-bahave
+if [ $? -ne 0 ]; then
+echo "error while installing"
+fi
+echo "behave installed succesfully"
+}
+cleanup_workspace()
+{
+echo "removing old behave files"
+sudo rm -r /var/lib/jenkins/workspace/testbehave/features
+if [ $? -ne 0 ]; then
+echo " error while removing"
+fi
+echo "behave files removed successfulyy"
+}
+behave_steps()
+{
+echo "steps to run BDD"
+sudo mkdir -p /var/lib/jenkins/workspace/testbehave/features/steps
+sudo cp -r ./test.feature /var/lib/jenkins/workspace/testbehave/features
+sudo cp -r ./test.py /var/lib/jenkins/workspace/testbehave/features/steps
+behave
+}
+python_install
+behave_install
+cleanup_workspace
+behave_steps
